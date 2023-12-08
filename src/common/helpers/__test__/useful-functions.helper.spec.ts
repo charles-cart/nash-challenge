318fc2248b2c8cdfa74de2dfa25af645cb547275
Charles Rodriguez <charles.rodriguez@externos-cl.cencosud.com>
2023-12-08 10:59:00 -0300

    test(toLocalString): add unit test

--- src/common/helpers/__test__/useful-functions.helper.spec.ts
+++ src/common/helpers/__test__/useful-functions.helper.spec.ts
@@ -5,4 +5,17 @@ describe('toLocalString', () => {
     const result = toLocalString();
     expect(typeof result).toBe('string');
   });
+
+  it('should return a string representing the current date and time', () => {
+    const result = toLocalString();
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    expect(
+      result.startsWith(
+        `${year}-${month < 10 ? '0' + month : month}-${day < 10 ? '0' + day : day}`,
+      ),
+    ).toBe(true);
+  });
 });
